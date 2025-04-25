8bae00134c14c30a9bc61bd54bb28a59c31d56f5
Bianca3489 <bia.rscr@hotmail.com>
2025-04-25 17:55:54 -0300

    Primeiro commit Luciano curso

---
+++ main.py
@@ -0,0 +1,58 @@
+print("Jornada de dados")
+
+# Variáveis
+jogos = 0
+
+# Tipos de dados
+# int, float, str, bool, list, tuple, dict, set, NoneType
+
+# int - inteiro
+# float - ponto flutuante   
+# str - string
+# bool - booleano (True ou False)
+# list - lista (array)
+# tuple - tupla (array imutável)
+# dict - dicionário (array associativo)
+# set - conjunto (array sem repetição)
+# NoneType - tipo nulo (None)
+
+
+# inteiros
+
+idade = 34          
+altura = 1.70
+
+# float
+peso = 70.5
+
+# string
+nome = 'Bia'
+
+# booleano
+ma = True
+
+# lista
+lista = [1, 2, 3, 4, 5]
+
+# tupla
+tupla = (1, 2, 3, 4, 5)
+
+# dicionário            
+dicionario = {'nome': 'Bia', 'idade': 34, 'altura': 1.70}
+
+# conjunto
+conjunto = {1, 2, 3, 4, 5}
+
+# NoneType
+nada = None
+
+print(idade)
+print(altura)
+print(peso)
+print(nome)    
+print(ma)
+print(lista)
+print(tupla)
+print(dicionario)
+print(conjunto)
+print(nada)
